212681dd7590843c87fa6f438adadb90f217f913
handrixn <handrian.ch@gmail.com>
2021-09-23 23:58:32 +0700

    initialize module

---
+++ database/mysql.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const db = {};
+const basename = 'index.js';
+const url = process.env.DB_CONNECTION_STRING;
+const dirname = `${__dirname}`;
+const timezoneOffset = require('../timezone.json');
+
+const config = {
+    dialect: 'mysql2',
+    dialectOptions: {
+        dateStrings: true,
+        decimalNumbers: true,
+        typeCast(field, next) {
+            if (field.type === 'DATETIME') {
+                return field.string();
+            }
+            return next();
+        }
+    },
+    timezone: (process.env.TZ ? (timezoneOffset[process.env.TZ] || '+07:00') : '+07:00'),
+    // eslint-disable-next-line no-console
+    logging: (process.env.DB_LOGGING === 'true' ? console.log : false),
+    operatorsAliases: 0,
+    define: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: 'deleted_at'
+    },
+    pool: {
+        max: (process.env.MYSQL_MAX_POOL ? parseInt(process.env.MYSQL_MAX_POOL) : 100),
+        min: 1,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+if (url) {
+    const sequelize = new Sequelize(url, config);
+
+    fs.readdirSync(dirname)
+        .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+        .forEach((file) => {
+            const model = require(path.join(dirname, file))(sequelize, Sequelize.DataTypes);
+            db[model.name] = model;
+        });
+
+    Object.keys(db).forEach((modelName) => {
+        if (db[modelName].associate) {
+            db[modelName].associate(db);
+        }
+    });
+
+    db.sequelize = sequelize;
+    db.Sequelize = Sequelize;
+    db.Op = Sequelize.Op;
+}
+
+module.exports = db;
---
+++ database/pgsql.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const db = {};
+const basename = 'index.js';
+const url = process.env.DB_CONNECTION_STRING;
+const dirname = `${__dirname}`;
+const timezoneOffset = require('../../config/timezone.json');
+
+const config = {
+    dialect: 'postgres',
+    dialectOptions: {
+        dateStrings: true,
+        decimalNumbers: true,
+        useUTC: false,
+        typeCast(field, next) {
+            if (field.type === 'DATETIME' || field.type === 'TIMESTAMP') {
+                return field.string();
+            }
+            return next();
+        }
+    },
+    timezone: (process.env.TZ ? (timezoneOffset[process.env.TZ] || '+02:00') : '+02:00'),
+    // eslint-disable-next-line no-console
+    logging: (process.env.DB_LOGGING === 'true' ? console.log : false),
+    operatorsAliases: 0,
+    define: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: 'deleted_at'
+    },
+    pool: {
+        max: (process.env.PGSQL_MAX_POOL ? parseInt(process.env.PGSQL_MAX_POOL) : 100),
+        min: 1,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+if (url) {
+    const sequelize = new Sequelize(url, config);
+
+    fs.readdirSync(dirname)
+        .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+        .forEach((file) => {
+            const model = require(path.join(dirname, file))(sequelize, Sequelize.DataTypes);
+            db[model.name] = model;
+        });
+
+    Object.keys(db).forEach((modelName) => {
+        if (db[modelName].associate) {
+            db[modelName].associate(db);
+        }
+    });
+
+    db.sequelize = sequelize;
+    db.Sequelize = Sequelize;
+    db.Op = Sequelize.Op;
+}
+
+module.exports = db;
